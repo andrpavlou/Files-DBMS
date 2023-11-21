f5d478918ec3b38aa80e8e64b6fd61315d2d9c96
andrpavlou <andreaspavlou2002@gmail.com>
2023-11-22 00:29:17 +0200

    open/close

--- HT_FILES/code/src/hash_file.c
+++ HT_FILES/code/src/hash_file.c
@@ -6,26 +6,28 @@
 #include "bf.h"
 #include "hash_file.h"
 #define MAX_OPEN_FILES 20
-#define HP_ERROR -1
+#define HT_ERROR -1
 
 
-int file_num = 0;
-int first_file_desc = 0;
 
 #define CALL_BF(call)       \
 {                           \
   BF_ErrorCode code = call; \
   if (code != BF_OK) {      \
     BF_PrintError(code);    \
-    return HP_ERROR;        \
+    return HT_ERROR;        \
   }                         \
 }
 
 
 typedef struct {
+	int file_desc;
 	int global_depth;
-  BF_Block** hash_table;
+  	BF_Block** hash_table;
 }Index_info;
+
+Index_info* open_files[MAX_OPEN_FILES];
+
 
 // typedef struct {
 // 	int local_depth;
@@ -40,52 +42,74 @@ HT_ErrorCode HT_Init(){
 
 HT_ErrorCode HT_CreateIndex(const char *filename, int depth) {
 	int file_desc;
-  BF_Block* block;
-  Index_info* index;
+  	BF_Block* block;
+  	Index_info* index;
 
 	CALL_BF(BF_CreateFile(filename)); 
 	CALL_BF(BF_OpenFile(filename, &file_desc));
 
-  BF_Block_Init(&block);
-  CALL_BF(BF_AllocateBlock(file_desc, block));
+  	BF_Block_Init(&block);
+  	CALL_BF(BF_AllocateBlock(file_desc, block));
 
-  void* data;
-  data = BF_Block_GetData(block);
+  	void* data;
+	data = BF_Block_GetData(block);
 
-  index = data;
-  index->global_depth = depth;
+  	index = data;
+  	index->global_depth = depth;
+	index->file_desc = file_desc;
   
-  int power = pow(2, depth);
+  	int power = pow(2, depth);
 
-  index->hash_table = malloc(power * sizeof(BF_Block*));
+  	index->hash_table = malloc(power * sizeof(BF_Block*));
   
 
-  BF_Block_Destroy(&block);
-  BF_Close(file_desc);
+  	BF_Block_Destroy(&block);
+  	BF_Close(file_desc);
 
-  return HT_OK;
+	return HT_OK;
 }
 
 HT_ErrorCode HT_OpenIndex(const char *fileName, int *indexDesc){
     BF_Block* block;
     BF_Block_Init(&block);
+
+
     int file_desc;
-    BF_OpenFile(fileName, &file_desc);
-
-    BF_GetBlock(file_desc, 0, block);
+    CALL_BF(BF_OpenFile(fileName, &file_desc));
+    
+    CALL_BF(BF_GetBlock(file_desc, 0, block));
     void* data;
-    data = BF_Block_GetData(block);
-    Index_info* index;
-    index = data;
-    
 
-    
-    return HT_OK;
+    data = BF_Block_GetData(block);
+    Index_info* info;
+    info = data;
+
+	for(int i = 0; i < MAX_OPEN_FILES; i++){
+		if(open_files[i] == NULL){
+			open_files[i] = info;
+			*indexDesc = i;
+			BF_Block_Destroy(&block);
+			return HT_OK;
+		}
+	}
+	BF_Block_Destroy(&block);
+    return HT_ERROR;
 }
 
 HT_ErrorCode HT_CloseFile(int indexDesc) {
-  //insert code here
-  return HT_OK;
+	//insert code here
+	///////////////////////////// set_dirty!?!?!??!?!?!?!
+	Index_info* index;
+	index = open_files[indexDesc];
+
+
+	open_files[indexDesc] = NULL;
+
+	free(index->hash_table);
+	
+	BF_CloseFile(index->file_desc);
+
+	return HT_OK;
 }
 
 HT_ErrorCode HT_InsertEntry(int indexDesc, Record record) {
